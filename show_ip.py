d752fae0051388b7e90b3bf2b9e606eeeb920931
Nick Huppert <s3729119@student.rmit.edu.au>
2020-03-17 17:21:07 +1100

    Added joystick interaction to quit program and keyboard interrupts now clear screen before exit

--- show_ip.py
+++ show_ip.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # Use: ps -ef | grep python
 # And then kill <pid> to stop this script.
+# Or use joystick in any fashion to exit program
 import sys, socket, sense_hat, time
 
-def IP_address():
+
+def ip_address():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("198.41.0.4", 53))
@@ -13,12 +15,25 @@ def IP_address():
     except socket.error:
         return None
 
-def display_IP_address():
-    sense.show_message("IPv4: " + str(IP_address()))
+
+def display_ip_address():
+    sense.show_message("IPv4: " + str(ip_address()))
+
 
 sense = sense_hat.SenseHat()
 
-while True:
+# Updated loop to exit when joystick is used and clear screen on interrupts from keyboard
+try:
+    joystick_used = False
+    while joystick_used is not True:
+        sense.clear()
+        display_ip_address()
+        # Check for events on joystick while message was scrolling
+        joystick_event_list = sense.stick.get_events()
+        if (len(joystick_event_list)) is not 0:
+            joystick_used = True
+        else:
+            time.sleep(10)
+
+except KeyboardInterrupt:
     sense.clear()
-    display_IP_address()
-    time.sleep(10)
